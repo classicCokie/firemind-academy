6c43a18095e2f7c40b77f8d362bccd9fa2146f06
Yannic <yannic@Yannics-MacBook-Pro.local>
2020-02-23 21:06:37 +0100

    feat: first try adding analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { initGA, logPageView } from '../services/analytics';
 
 class MyDocument extends Document {
+    componentDidMount() {
+        // @ts-ignore
+        if (!window.GA_INITIALIZED) {
+            initGA()
+            // @ts-ignore
+            window.GA_INITIALIZED = true
+        }
+        logPageView()
+    }
+
+
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
@@ -10,6 +22,8 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-82042429-5"></script>
+
                     <link
                         href="https://fonts.googleapis.com/css?family=Lato:900&display=swap"
                         rel="stylesheet"
